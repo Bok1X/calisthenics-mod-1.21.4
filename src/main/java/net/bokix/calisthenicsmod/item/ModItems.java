dc9091c1364423e46aa782d916002eec7eacf430
BokiX423 <otacisin0077@gmail.com>
2025-03-20 01:47:24 +0100

    i stole copper to make this mod

---
+++ src/main/java/net/bokix/calisthenicsmod/item/ModItems.java
@@ -0,0 +1,15 @@
+package net.bokix.calisthenicsmod.item;
+
+import net.bokix.calisthenicsmod.CalisthenicsMod;
+
+public class ModItems {
+
+
+
+
+
+
+    public static void registerModItems() {
+        CalisthenicsMod.LOGGER.info("Registering Mod Items for " + CalisthenicsMod.MOD_ID);
+    }
+}
