f33640670b3882246bbcf801499a97508ffd5b4c
BokiX423 <otacisin0077@gmail.com>
2025-03-20 01:14:36 +0100

    Initial commit

---
+++ src/main/java/net/bokix/calisthenicsmod/CalisthenicsMod.java
@@ -0,0 +1,16 @@
+package net.bokix.calisthenicsmod;
+
+import net.fabricmc.api.ModInitializer;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class CalisthenicsMod implements ModInitializer {
+	public static final String MOD_ID = "calisthenicsmod";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/bokix/calisthenicsmod/CalisthenicsModClient.java
@@ -0,0 +1,10 @@
+package net.bokix.calisthenicsmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class CalisthenicsModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
